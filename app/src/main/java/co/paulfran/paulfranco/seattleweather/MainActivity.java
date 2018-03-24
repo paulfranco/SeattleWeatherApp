852500efb149bc129ec8d59d815f26fdbcf27fd0
Paul Franco <paulfranco@me.com>
2018-03-24 00:35:10 -0700

    Added AlertDialogFragment

--- app/src/main/java/co/paulfran/paulfranco/seattleweather/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/seattleweather/MainActivity.java
@@ -2,12 +2,61 @@ package co.paulfran.paulfranco.seattleweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "6a70dc406b820349630a41ac4ce68967";
+        double latitude = 37.8267;
+        double longitude =  -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.v(TAG, response.body().string());
+                try {
+
+                    if (response.isSuccessful()) {
+                    }
+                    else {
+                        alertUserAboutError();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+        Log.d(TAG, "Main UI us running!");
+
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
