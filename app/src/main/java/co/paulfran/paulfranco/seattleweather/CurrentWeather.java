d334d1db41ba7e30e027d082bef8a43e4ce32e9c
Paul Franco <paulfranco@me.com>
2018-03-24 23:55:44 -0700

    Added CurrentWeather Class

---
+++ app/src/main/java/co/paulfran/paulfranco/seattleweather/CurrentWeather.java
@@ -0,0 +1,62 @@
+package co.paulfran.paulfranco.seattleweather;
+
+/**
+ * Created by paulfranco on 3/24/18.
+ */
+
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getmIcon() {
+        return mIcon;
+    }
+
+    public void setmIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public long getmTime() {
+        return mTime;
+    }
+
+    public void setmTime(long mTime) {
+        this.mTime = mTime;
+    }
+
+    public double getmTemperature() {
+        return mTemperature;
+    }
+
+    public void setmTemperature(double mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public double getmHumidity() {
+        return mHumidity;
+    }
+
+    public void setmHumidity(double mHumidity) {
+        this.mHumidity = mHumidity;
+    }
+
+    public double getmPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setmPrecipChance(double mPrecipChance) {
+        this.mPrecipChance = mPrecipChance;
+    }
+
+    public String getmSummary() {
+        return mSummary;
+    }
+
+    public void setmSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+}
