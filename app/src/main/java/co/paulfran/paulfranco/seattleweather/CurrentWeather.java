85c65ed6a45ae7a3efaf378ec0e792bd876aef6c
Paul Franco <paulfranco@me.com>
2018-03-25 13:32:29 -0700

    added Icon Conditions

--- app/src/main/java/co/paulfran/paulfranco/seattleweather/CurrentWeather.java
+++ app/src/main/java/co/paulfran/paulfranco/seattleweather/CurrentWeather.java
@@ -1,5 +1,9 @@
 package co.paulfran.paulfranco.seattleweather;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by paulfranco on 3/24/18.
  */
@@ -11,17 +15,73 @@ public class CurrentWeather {
     private double mHumidity;
     private double mPrecipChance;
     private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
 
     public String getmIcon() {
         return mIcon;
     }
 
+
     public void setmIcon(String mIcon) {
         this.mIcon = mIcon;
     }
 
+    public int getIconId() {
+
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+        return iconId;
+    }
+
     public long getmTime() {
         return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formater = new SimpleDateFormat("h:mm a");
+        formater.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date datetime = new Date(getmTime() * 1000);
+        String timeString = formater.format(datetime);
+
+        return timeString;
     }
 
     public void setmTime(long mTime) {
